0af99b3c0268dba9984540292350545606a27ba9
Chinatsu Kanayama <c.kanayama@everyleaf.com>
2023-09-27 22:43:48 +0900

    basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production? 
+
+  private
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |name, password|
+      name == ENV["BASIC_AUTH_NAME"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
