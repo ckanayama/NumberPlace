e3c078246e265bb5042ada46275fc036e5a86715
Chinatsu Kanayama <c.kanayama@everyleaf.com>
2023-09-28 10:02:01 +0900

    動線と共通スタイルを整備

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "puzzles#index"
+  root "pages#home"
 
   resources :puzzles, only: [:index, :show, :create, :update]
 end
