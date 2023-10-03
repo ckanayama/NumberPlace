daed9ed83469929de8ecdb83ee00e352224ffee2
Chinatsu Kanayama <c.kanayama@everyleaf.com>
2023-10-03 17:49:11 +0900

    答え合わせを改善

--- app/helpers/puzzle_helper.rb
+++ app/helpers/puzzle_helper.rb
@@ -1,2 +1,8 @@
 module PuzzleHelper
+  def wrong_answer?(index, column)
+    return false unless @puzzle.status == 'submitted' 
+    return false if column.blank?
+
+    @puzzle.number.correct_answer_array[index] != column
+  end
 end
