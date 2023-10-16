209ffb77ade5023a1c6bc3b698df39929247966b
Chinatsu Kanayama <c.kanayama@everyleaf.com>
2023-10-16 22:19:39 +0900

    maruを動かせるようにする

--- app/javascript/canvas/maru.js
+++ app/javascript/canvas/maru.js
@@ -1,26 +1,40 @@
 window.addEventListener('load', function() {
-  draw();
+  const canvas = document.getElementById("maru");
+
+  draw(canvas);
+
+  const mouseMove = (event) => {
+    canvas.style.top = (event.clientY - 30) + "px";
+    canvas.style.left = (event.clientX - 30) + "px";
+  }
+
+  canvas.onmousedown = function() {
+    canvas.addEventListener('mousemove', mouseMove);
+  };
+
+  canvas.onmouseup = function() {
+    canvas.removeEventListener('mousemove', mouseMove);
+  }
 });
 
-function draw(x) {
-  var canvas = document.getElementById("maru");
-  canvas.width = 120;
-  canvas.height = 120;
-  var ctx = canvas.getContext("2d");
-  ctx.clearRect(0,0, canvas.width, canvas.height);
+function draw(canvas) {
+  canvas.width = 60;
+  canvas.height = 60;
+  let ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   ctx.beginPath();
   ctx.fillStyle = "#fff";
-  ctx.arc(60, 100, 30, 0, 2 * Math.PI);
+  ctx.arc(30, 30, 30, 0, 2 * Math.PI);
   ctx.fill();
 
   ctx.beginPath();
   ctx.fillStyle = "#111";
-  ctx.arc(46, 91, 3, 0, 2 * Math.PI);
+  ctx.arc(15, 20, 3, 0, 2 * Math.PI);
   ctx.fill();
 
   ctx.beginPath();
   ctx.fillStyle = "#111";
-  ctx.arc(68, 91, 3, 0, 2 * Math.PI);
+  ctx.arc(33, 20, 3, 0, 2 * Math.PI);
   ctx.fill();
 }
