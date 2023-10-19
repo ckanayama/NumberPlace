a8e302d6d1ca41b6a19dc901595464f211d687e0
Chinatsu Kanayama <c.kanayama@everyleaf.com>
2023-10-19 21:12:49 +0900

    空欄の場合も含めて間違いの箇所を知りたいため

--- app/helpers/puzzle_helper.rb
+++ app/helpers/puzzle_helper.rb
@@ -1,7 +1,6 @@
 module PuzzleHelper
   def wrong_answer?(index, column)
     return false unless @puzzle.status == 'submitted' 
-    return false if column.blank?
 
     @puzzle.number.correct_answer_array[index] != column
   end
